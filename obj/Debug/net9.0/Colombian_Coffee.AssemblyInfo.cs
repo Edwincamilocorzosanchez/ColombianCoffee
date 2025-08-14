dbd47a17b75fbc65f732251357b22848342da43a
Edwin camilo corzo sanchez <194437744+Edwincamilocorzosanchez@users.noreply.github.com.>
2025-08-14 11:02:18 -0500

    feat: :fire: Creacion de la carpeta Helpers y sus respectivas clases DbContextFactory, MySqlVersion

--- obj/Debug/net9.0/Colombian_Coffee.AssemblyInfo.cs
+++ obj/Debug/net9.0/Colombian_Coffee.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Colombian_Coffee")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dc5d55870fbf2cf6e202f718e19d031f987b2d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fbd8651f8f556b72787a6896bd60e8c673ec567")]
 [assembly: System.Reflection.AssemblyProductAttribute("Colombian_Coffee")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Colombian_Coffee")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
