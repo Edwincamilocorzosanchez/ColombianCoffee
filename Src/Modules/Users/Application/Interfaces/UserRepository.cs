97dc613391848028d6f1febb0c8e047a1e03a24c
Edwin camilo corzo sanchez <194437744+Edwincamilocorzosanchez@users.noreply.github.com.>
2025-08-14 10:45:30 -0500

    feat: :fire: Creacion de carpetas Src, Modules, Application, Interfaces e interfas UserRepository

---
+++ Src/Modules/Users/Application/Interfaces/UserRepository.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Colombian_Coffee.Src.Modules.Users.Application.Interfaces
+{
+    public interface UserRepository
+    {
+        
+    }
+}
